5785cce2e67e2bf7cc514a6bac0fcb8f958aa01a
Evgeny <example@gmail.com>
2024-03-22 12:52:44 +0300

    new component

---
+++ src/components/gitTestComponent/GitTest.tsx
@@ -0,0 +1,14 @@
+
+
+// компонент создан только для теста git к проекту он никак не относится!!!!!!
+
+const GitTest = () => {
+
+    return (
+        <div>
+            git
+        </div>
+    )
+}
+
+export default GitTest;
